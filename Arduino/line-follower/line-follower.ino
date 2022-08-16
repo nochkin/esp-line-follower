f8067898718b504e985b8e4f243c122965146c01
nochkin <github@durdom.com>
2022-08-16 15:10:27 -0400

    Revert "TB6612 support"
    This reverts commit 02e79df51a6babfc2041ab42208772d88f71af53.

--- Arduino/line-follower/line-follower.ino
+++ Arduino/line-follower/line-follower.ino
@@ -1,72 +1,56 @@
-#define IR1 13 // D7
+#define IR1 14 // D5
 #define IR2 12 // D6
-// 12 // D6
 // 13 // D7
-// 14 // D5
 // 15 // D8
 
-#define MOTOR_L_PWM 4
-#define MOTOR_L_IN1 2
-#define MOTOR_L_IN2 16
+#define MOTOR_L_PWM 5 // D1
+#define MOTOR_L_DIR 0 // D3
+#define MOTOR_R_PWM 4 // D2
+#define MOTOR_R_DIR 2 // D4
 
-#define MOTOR_R_PWM 5
-#define MOTOR_R_IN1 0
-#define MOTOR_R_IN2 17
-
-#define MOTORS_ENABLE 32
-
-#define SPEED_MAX 140
+#define SPEED_MAX 1023
 
 #ifdef ARDUINO_ARCH_ESP32
-#include <pwmWrite.h>
+#include "analogWrite.h"
 #endif
-
-Pwm pwm = Pwm();
 
 class Motor {
   public:
-    Motor(uint8_t, uint8_t, uint8_t);
-    Motor(uint8_t, uint8_t, uint8_t, bool);
+    Motor(uint8_t, uint8_t);
+    Motor(uint8_t, uint8_t, bool);
     void forward(uint16_t);
     void reverse(uint16_t);
     void stop();
   protected:
     void set_speed(uint16_t);
-    uint8_t pwm_pin, in1_pin, in2_pin;
+    uint8_t pwm_pin, dir_pin;
     bool inv;
 };
 
-Motor::Motor(uint8_t in_pwm, uint8_t in1, uint8_t in2) {
-  Motor(in_pwm, in1, in2, 0);
+Motor::Motor(uint8_t pwm, uint8_t direction) {
+  Motor(pwm, direction, 0);
 }
 
-Motor::Motor(uint8_t in_pwm, uint8_t in1, uint8_t in2, bool inverse) {
-  pwm_pin = in_pwm;
-  in1_pin = in1;
-  in2_pin = in2;
+Motor::Motor(uint8_t pwm, uint8_t direction, bool inverse) {
+  pwm_pin = pwm;
+  dir_pin = direction;
   inv = inverse;
-  pinMode(in1_pin, OUTPUT);
-  pinMode(in2_pin, OUTPUT);
+  pinMode(pwm_pin, OUTPUT);
+  pinMode(dir_pin, OUTPUT);
   this->stop();
 }
 
 void Motor::set_speed(uint16_t speed) {
-#ifdef ARDUINO_ARCH_ESP32
-  pwm.write(pwm_pin, speed > 255 ? 255 : speed);
-#else
-  analogWrite(pwm_pin, speed > 255 ? 255 : speed);
-#endif
+  analogWrite(pwm_pin, speed > 1023 ? 1023 : speed);
 }
 
 void Motor::forward(uint16_t speed) {
-  digitalWrite(in1_pin, inv ? HIGH : LOW);
-  digitalWrite(in2_pin, inv ? LOW : HIGH);
+  digitalWrite(dir_pin, inv ? HIGH : LOW);
   this->set_speed(speed);
 }
 
 void Motor::reverse(uint16_t speed) {
-  digitalWrite(in1_pin, inv ? LOW : HIGH);
-  digitalWrite(in2_pin, inv ? HIGH : LOW);
+  digitalWrite(dir_pin, inv ? LOW : HIGH);
   this->set_speed(speed);
 }
 
@@ -74,19 +58,14 @@ void Motor::stop() {
   this->set_speed(0);
 }
 
-Motor left(MOTOR_L_PWM, MOTOR_L_IN1, MOTOR_L_IN2, true);
-Motor right(MOTOR_R_PWM, MOTOR_R_IN1, MOTOR_R_IN2, true);
+Motor left(MOTOR_L_PWM, MOTOR_L_DIR, false);
+Motor right(MOTOR_R_PWM, MOTOR_R_DIR, true);
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(IR1, INPUT);
   pinMode(IR2, INPUT);
-
-  delay(1000);
-  
-  pinMode(MOTORS_ENABLE, OUTPUT);
-  digitalWrite(MOTORS_ENABLE, HIGH);
 }
 
 void loop() {
