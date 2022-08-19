2128178319acba108ccbbcb896d32438a2eece38
nochkin <github@durdom.com>
2022-08-19 01:09:48 -0400

    Use ledc instead of pwm for ESP32

--- Arduino/line-follower/line-follower.ino
+++ Arduino/line-follower/line-follower.ino
@@ -5,15 +5,17 @@
 // Pins configuration
 #ifdef ARDUINO_ARCH_ESP32
 // Pins configuration for ESP32
-#define IR1 13
-#define IR2 12
+#define IR_L 12
+#define IR_R 13
 
 #if defined(TB6612)
 #define MOTOR_L_PWM 4
+#define MOTOR_L_PWM_CH 0
 #define MOTOR_L_IN1 2
 #define MOTOR_L_IN2 16
 
 #define MOTOR_R_PWM 5
+#define MOTOR_R_PWM_CH 2
 #define MOTOR_R_IN1 0
 #define MOTOR_R_IN2 17
 
@@ -25,8 +27,8 @@
 #define SPEED_MAX 140
 #else
 // Pins configuration for ESP8266
-#define IR1 14 // D5
-#define IR2 12 // D6
+#define IR_L 14 // D5
+#define IR_R 12 // D6
 
 #if defined(L293DD)
 #define MOTOR_L_PWM 5 // D1
@@ -38,11 +40,6 @@
 #endif
 
 #define SPEED_MAX 1023
-#endif
-
-#ifdef ARDUINO_ARCH_ESP32
-#include <pwmWrite.h>
-Pwm pwm = Pwm();
 #endif
 
 class MotorL293DD {
@@ -58,12 +55,12 @@ class MotorL293DD {
     bool inv;
 };
 
-MotorL293DD::MotorL293DD(uint8_t pwm, uint8_t direction) {
-  MotorL293DD(pwm, direction, 0);
+MotorL293DD::MotorL293DD(uint8_t pwm_pin, uint8_t direction) {
+  MotorL293DD(pwm_pin, direction, 0);
 }
 
-MotorL293DD::MotorL293DD(uint8_t pwm, uint8_t direction, bool inverse) {
-  pwm_pin = pwm;
+MotorL293DD::MotorL293DD(uint8_t pwm_pin, uint8_t direction, bool inverse) {
+  pwm_pin = pwm_pin;
   dir_pin = direction;
   inv = inverse;
   pinMode(pwm_pin, OUTPUT);
@@ -91,34 +88,39 @@ void MotorL293DD::stop() {
 
 class MotorTB6612 {
   public:
-    MotorTB6612(uint8_t, uint8_t, uint8_t);
-    MotorTB6612(uint8_t, uint8_t, uint8_t, bool);
+    MotorTB6612(uint8_t, uint8_t, uint8_t, uint8_t);
+    MotorTB6612(uint8_t, uint8_t, uint8_t, uint8_t, bool);
     void forward(uint16_t);
     void reverse(uint16_t);
     void stop();
   protected:
     void set_speed(uint16_t);
-    uint8_t pwm_pin, in1_pin, in2_pin;
+    uint8_t pwm_pin, pwm_ch, in1_pin, in2_pin;
     bool inv;
 };
 
-MotorTB6612::MotorTB6612(uint8_t in_pwm, uint8_t in1, uint8_t in2) {
-  MotorTB6612(in_pwm, in1, in2, 0);
+MotorTB6612::MotorTB6612(uint8_t in_pwm, uint8_t in_pwm_ch, uint8_t in1, uint8_t in2) {
+  MotorTB6612(pwm_pin, pwm_ch, in1, in2, 0);
 }
 
-MotorTB6612::MotorTB6612(uint8_t in_pwm, uint8_t in1, uint8_t in2, bool inverse) {
+MotorTB6612::MotorTB6612(uint8_t in_pwm, uint8_t in_pwm_ch, uint8_t in1, uint8_t in2, bool inverse) {
   pwm_pin = in_pwm;
+  pwm_ch = in_pwm_ch;
   in1_pin = in1;
   in2_pin = in2;
   inv = inverse;
   pinMode(in1_pin, OUTPUT);
   pinMode(in2_pin, OUTPUT);
+#ifdef ARDUINO_ARCH_ESP32
+  ledcSetup(pwm_ch, 5000, 8);
+  ledcAttachPin(pwm_pin, pwm_ch);
+#endif
   this->stop();
 }
 
 void MotorTB6612::set_speed(uint16_t speed) {
 #ifdef ARDUINO_ARCH_ESP32
-  pwm.write(pwm_pin, speed > 255 ? 255 : speed);
+  ledcWrite(pwm_ch, speed > 255 ? 255 : speed);
 #else
   analogWrite(pwm_pin, speed > 255 ? 255 : speed);
 #endif
@@ -144,8 +146,8 @@ void MotorTB6612::stop() {
 MotorL293DD left(MOTOR_L_PWM, MOTOR_L_DIR, false);
 MotorL293DD right(MOTOR_R_PWM, MOTOR_R_DIR, true);
 #elif defined(TB6612)
-MotorTB6612 left(MOTOR_L_PWM, MOTOR_L_IN1, MOTOR_L_IN2, true);
-MotorTB6612 right(MOTOR_R_PWM, MOTOR_R_IN1, MOTOR_R_IN2, true);
+MotorTB6612 left(MOTOR_L_PWM, MOTOR_L_PWM_CH, MOTOR_L_IN1, MOTOR_L_IN2, true);
+MotorTB6612 right(MOTOR_R_PWM, MOTOR_R_PWM_CH, MOTOR_R_IN1, MOTOR_R_IN2, true);
 #else
 #error "Selected motor driver not supported"
 #endif
@@ -153,8 +155,8 @@ MotorTB6612 right(MOTOR_R_PWM, MOTOR_R_IN1, MOTOR_R_IN2, true);
 void setup() {
   Serial.begin(115200);
 
-  pinMode(IR1, INPUT);
-  pinMode(IR2, INPUT);
+  pinMode(IR_L, INPUT);
+  pinMode(IR_R, INPUT);
 
   delay(1000);
 
@@ -165,27 +167,27 @@ void setup() {
 }
 
 void loop() {
-  uint16_t ir1 = analogRead(IR1);
-  uint16_t ir2 = analogRead(IR2);
+  uint16_t ir_l = digitalRead(IR_L);
+  uint16_t ir_r = digitalRead(IR_R);
 
-  if ((ir1 < 500) && (ir2 < 500)) {
+  if ((ir_l == LOW) && (ir_r == LOW)) {
     // both sensors are on white
     left.forward(SPEED_MAX);
     right.forward(SPEED_MAX);
-    
-  } else if (ir1 < 500) {
+
+  } else if ((ir_l == HIGH) && (ir_r == HIGH)) {
+    // both sensors are on black, stop
+    left.stop();
+    right.stop();
+
+  } else if (ir_l == LOW) {
     // left sensor is on white, right is on the line
     left.forward(SPEED_MAX);
     right.reverse(SPEED_MAX/1.3);
     
-  } else if (ir2 < 500) {
+  } else if (ir_r == LOW) {
     // right sensor is on white, left is on the line
     left.reverse(SPEED_MAX/1.3);
     right.forward(SPEED_MAX);
-    
-  } else {
-    // both sensors are on black, stop
-    left.stop();
-    right.stop();
   }
 }
